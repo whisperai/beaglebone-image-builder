2e63666bd5ce5d2b11a7dcfe20759273f2b5fde8
Michael Liberato <mliberato@whisper.ai>
2022-07-29 17:43:29 -0700

    make it sudo

--- scripts/whisper_chroot.sh
+++ scripts/whisper_chroot.sh
@@ -34,9 +34,9 @@ __EOF__
 
 install_custom_python () {
   echo "Log: Installing custom whisper bbb python"
-  cp -r ${DIR}/custom_python ${tempdir}
+  sudo cp -r ${DIR}/custom_python ${tempdir}
   sudo chroot "${tempdir}" python -m pip install ./custom_python
-  rm -rf ${tempdir}/custom_python
+  sudo rm -rf ${tempdir}/custom_python
 }
 
 enable_i2c1_on_startup
