6930a1017b29fd40b3c6566b8594dcd56f2022bb
Michael Liberato <mliberato@whisper.ai>
2022-07-29 17:35:08 -0700

    remove leading spaces

--- scripts/whisper_chroot.sh
+++ scripts/whisper_chroot.sh
@@ -5,24 +5,24 @@
 
 enable_i2c1_on_startup () {
   cat > ${tempdir}/usr/local/src/set_pins.sh <<-__EOF__
-    #!/bin/bash -e
+#!/bin/bash -e
 
-    # Config the i2c1 PINs
-    config-pin p9.24 i2c
-    config-pin p9.26 i2c
-  __EOF__
+# Config the i2c1 PINs
+config-pin p9.24 i2c
+config-pin p9.26 i2c
+__EOF__
   sudo chmod +x ${tempdir}/usr/local/src/set_pins.sh
   cat > ${tempdir}/etc/systemd/system/set_pins.service <<-__EOF__
-    [Unit]
-    Description=Setup for BBB pins
+[Unit]
+Description=Setup for BBB pins
 
-    [Service]
-    Type=simple
-    ExecStart=/bin/bash /usr/local/src/set_pins.sh
+[Service]
+Type=simple
+ExecStart=/bin/bash /usr/local/src/set_pins.sh
 
-    [Install]
-    WantedBy=multi-user.target
-  __EOF__
+[Install]
+WantedBy=multi-user.target
+__EOF__
 
   sudo chroot "${tempdir}" systemctl start set_pins.service
   sudo chroot "${tempdir}" systemctl enable set_pins.service
