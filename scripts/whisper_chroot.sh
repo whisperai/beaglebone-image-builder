af2f9cfdd1246880640b4b10791adefb4dc520b3
Michael Liberato <mliberato@whisper.ai>
2022-07-31 19:26:11 -0700

    sudo + organization

--- scripts/whisper_chroot.sh
+++ scripts/whisper_chroot.sh
@@ -4,28 +4,32 @@
 # This could be expanded to different BBB images in the future.
 
 enable_i2c1_on_startup () {
-  cat > ${tempdir}/usr/local/src/set_pins.sh <<-__EOF__
+  # Make the pin configuration script
+  cat > configure_pins.sh <<-__EOF__
 #!/bin/bash -e
 
 # Config the i2c1 PINs
 config-pin p9.24 i2c
 config-pin p9.26 i2c
 __EOF__
-  sudo chmod +x ${tempdir}/usr/local/src/set_pins.sh
-  cat > ${tempdir}/etc/systemd/system/set_pins.service <<-__EOF__
+  sudo chmod +x configure_pins.sh
+  sudo mv configure_pins.sh ${tempdir}/home/debian
+
+  # Use systemctl to configure pins on startup
+  cat > configure_pins.service <<-__EOF__
 [Unit]
 Description=Setup for BBB pins
 
 [Service]
 Type=simple
-ExecStart=/bin/bash /usr/local/src/set_pins.sh
+ExecStart=/bin/bash /home/debian/configure_pins.sh
 
 [Install]
 WantedBy=multi-user.target
 __EOF__
-
-  sudo chroot "${tempdir}" systemctl start set_pins.service
-  sudo chroot "${tempdir}" systemctl enable set_pins.service
+  sudo mv configure_pins.service ${tempdir}/etc/systemd/system
+  sudo chroot "${tempdir}" systemctl start configure_pins.service
+  sudo chroot "${tempdir}" systemctl enable configure_pins.service
 }
 
 enable_i2c1_on_startup
